fd80899b1c82684d585c2a2c5018bdf42d862d4b
ChungjunLee <21400608@handong.edu>
2019-02-15 23:09:27 +0900

    model Re-Loading is implemented
    within train.py:
    	1) added argparse argument --isReload
    		if it is specified, the model re-loads model options and model parameters from files previously saved
    
    	2) modified variable names throughout the file : args.<variableName> -> <variableName>
    		this change was necessary for implementing re-load ability.

--- train.py
+++ train.py
@@ -18,6 +18,8 @@ parser.add_argument("--data_prefix", type=str, default='unsw', help="str, prefix
 
 # for model saving
 parser.add_argument("--saveto", type=str, default='saveto', help="str, During training, the model writes its parameters into a file whenever the model's validation score imporves. 'saveto' is a string to include in the name of that file (in addition to prefix)")
+parser.add_argument("--isReload", dest='isReload', action='store_true', help="no argument, when this option is specified the model will re-load existing model from files. Only possible when the model with the same 'data_prefix' and 'saveto' had been training previously. The model will simply adopt all training, model, and other parameters from saved files.")
+parser.set_defaults(isReload=True)
 
 # model architecture
 parser.add_argument("--num_hidden", type=int, default=4, help="int, must be an integer within the range [1, 4]")
@@ -38,17 +40,28 @@ parser.add_argument("--patience", type=int, default=10, help="int")
 
 args = parser.parse_args()
 
+# in case isReload load model_options file
+import _pickle as pkl
+if args.isReload:
+	with open(args.data_prefix + "." + args.saveto + ".model_options.pkl", "rb") as fp:
+		model_options = pkl.load(fp)
+	globals().update(model_options)
+else:
+	model_options = vars(args)
+	globals().update(model_options)
+	with open(data_prefix + "." + saveto + ".model_options.pkl", "wb") as fp: # write new model_options file
+		pkl.dump(model_options, fp)
+
 # load dataset and create dataloader from pkl file
 print("importing datasets from pkl file...")
 import pandas as pd
-import _pickle as pkl
 from model import TabularDataset
 from torch.utils.data import DataLoader
 
-df_train = pd.read_pickle(args.data_prefix + ".tr.pkl")
-df_valid = pd.read_pickle(args.data_prefix + ".val.pkl")
+df_train = pd.read_pickle(data_prefix + ".tr.pkl")
+df_valid = pd.read_pickle(data_prefix + ".val.pkl")
 
-with open(args.data_prefix + ".cols.pkl", "rb") as fp:
+with open(data_prefix + ".cols.pkl", "rb") as fp:
 	cols_dict = pkl.load(fp)
 if cols_dict["cat_cols"] == None:
 	cols_dict["cat_cols"] = []
@@ -57,8 +70,8 @@ print("preparing dataloader")
 trainset = TabularDataset(df_train, cat_cols=cols_dict["cat_cols"], output_col=cols_dict["output_cols"])
 validset = TabularDataset(df_valid, cat_cols=cols_dict["cat_cols"], output_col=cols_dict["output_cols"])
 
-trainloader = DataLoader(trainset, batch_size=args.batch_size, shuffle=True, num_workers=1)
-validloader = DataLoader(validset, batch_size=args.batch_size, shuffle=True, num_workers=1)
+trainloader = DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=1)
+validloader = DataLoader(validset, batch_size=batch_size, shuffle=True, num_workers=1)
 
 # build model
 print("building model...")
@@ -69,44 +82,44 @@ from model import FeedForwardNN
 cat_dims = cols_dict["cat_dims"]
 emb_dims = [(cat_dim, min(10, cat_dim // 2)) for cat_dim in cat_dims]
 no_of_cont = len(cols_dict["cont_cols"])
-lin_layer_sizes = [args.hidden1, args.hidden2, args.hidden3, args.hidden4]
+lin_layer_sizes = [hidden1, hidden2, hidden3, hidden4]
 
 #cat_dims = [df_train[col].nunique() for col in categorical_features]
 #emb_dims = [(cat_dim, min(10, cat_dim // 2)) for cat_dim in cat_dims]
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model = FeedForwardNN(emb_dims, no_of_cont=no_of_cont, lin_layer_sizes=lin_layer_sizes[:args.num_hidden], output_size=args.output_dim).to(device)
+model = FeedForwardNN(emb_dims, no_of_cont=no_of_cont, lin_layer_sizes=lin_layer_sizes[:num_hidden], output_size=output_dim).to(device)
 
-# print model_option also as a file
-model_options = vars(args)
 print(model_options)
 print(model)
-with open(args.data_prefix + "." + args.saveto + ".model_options.pkl", "wb") as fp:
-	pkl.dump(vars(args), fp)
+
+# case isReolad, reload model parameters
+if args.isReload:
+	model.load_state_dict(torch.load(data_prefix + "." + saveto + ".model_best.pkl"))
 
 # train for max_epochs
 print("training...")
-optimizer = "torch.optim." + args.optimizer
-loss = "torch.nn." + args.loss
-optimizer = eval(optimizer)(model.parameters(), lr=args.lr)
+optimizer = "torch.optim." + optimizer
+loss = "torch.nn." + loss
+optimizer = eval(optimizer)(model.parameters(), lr=lr)
 criterion = eval(loss)()
 
-model.train(data_prefix=args.data_prefix,
-	saveto=args.saveto,
+model.train(data_prefix=data_prefix,
+	saveto=saveto,
 	trainloader=trainloader,
 	validloader=validloader,
 	device=device,
-	max_epochs=args.max_epochs,
+	max_epochs=max_epochs,
 	criterion=criterion,
 	optimizer=optimizer,
-	validFreq=args.validFreq,
-	patience=args.patience)
+	validFreq=validFreq,
+	patience=patience)
 
 model.test(testloader=trainloader,
-	data_prefix=args.data_prefix,
-	loadfrom=args.saveto,
+	data_prefix=data_prefix,
+	loadfrom=saveto,
 	device=device)
 model.test(testloader=validloader,
-	data_prefix=args.data_prefix,
-	loadfrom=args.saveto,
+	data_prefix=data_prefix,
+	loadfrom=saveto,
 	device=device)
